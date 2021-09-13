0c56f8e7c0a828a9c84ae0b81a318be508644e06
mauri <utn.mauri@gmail.com>
2021-09-13 10:37:41 -0300

    Desafio Promise, Asincronia & Map

--- app-DesafioNavBar/store-app/src/components/Item/Item.jsx
+++ app-DesafioNavBar/store-app/src/components/Item/Item.jsx
@@ -1,28 +1,47 @@
 import './ItemStyle.css'
 import ItemCount from '../ItemCount/ItemCount';
+import { useState, useEffect } from 'react';
 import impulsivos from '../../productos.js'
 
+ /* ------ promise para mostrar los productos con 3 segundos de retraso ------ */
+const products = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if (impulsivos != null) {
+            resolve(impulsivos)
+        } else {
+            reject(() => console.log('algo anda mal'))
+        }
+    }, 3000)
+})
+
 const Item = () => {
+    const [productos, setproductos] = useState([]);
+    products
+        .then((result) => {
+            setproductos(result)
+        })
+        .catch((error) => console.log(error))
+        console.log(productos)
     const onAdd = (c) => {
         console.log(c)
     }
     return (
         <>
-         <div className='catalogo'>
-            {impulsivos.map((pd) =>
-                <div className='card '>
-                    <img  src={pd.img} className="card-img-top" alt="Bombon Escoces caja" />
-                    <div className="card-body">
-                        <h5 className="card-title"> {pd.descricpion}</h5>
-                        <p className="card-text" >Descripcion:{pd.descricpion}</p>
-                        <p className="card-text" >Stock: {pd.stock}</p>
-                        <p className="card-text" id="precio">Precio ${pd.Precio}</p>
-                        <a href="#algo" className="btn btn-primary" id="comprar">Ver Detalle</a>
+            <div className='catalogo'>
+                {productos.map((pd) =>
+                    <div className='card '>
+                        <img src={pd.img} className="card-img-top" alt="Bombon Escoces caja" />
+                        <div className="card-body">
+                            <h5 className="card-title"> {pd.descricpion}</h5>
+                            <p className="card-text" >Descripcion:{pd.descricpion}</p>
+                            <p className="card-text" >Stock: {pd.stock}</p>
+                            <p className="card-text" id="precio">Precio ${pd.Precio}</p>
+                            <a href="#algo" className="btn btn-primary" id="comprar">Ver Detalle</a>
+                        </div>
+                        <ItemCount stock={pd.stock} initial={1} onAdd={onAdd} />
                     </div>
-                    <ItemCount stock={pd.stock} initial={1} onAdd={onAdd} />
-                </div>
-            )}
-        </div>
+                )}
+            </div>
         </>
     )
 }
